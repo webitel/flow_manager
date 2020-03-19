993981dfd3e0494c411f4d1e3081117704702028
navrotskyj <navrotskyj@gmail.com>
2020-03-19 10:48:18 +0200

    print version

--- server.go
+++ server.go
@@ -6,21 +6,19 @@ import (
 	"github.com/webitel/flow_manager/call"
 	"github.com/webitel/flow_manager/flow"
 	"github.com/webitel/flow_manager/grpc_route"
-	"github.com/webitel/wlog"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
+	fmt.Println(app.Version())
 	interruptChan := make(chan os.Signal, 1)
-
 	fm, err := app.NewFlowManager()
 
 	if err != nil {
 		panic(err.Error())
 	}
-	wlog.Info(fmt.Sprintf("server build version: %s", fm.Version()))
 
 	flow.Init(fm)
 	call.Init(fm)
