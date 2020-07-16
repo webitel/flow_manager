ed0774741c4800638a0bdbdd4c54fff9dbc2c6bb
navrotskyj <navrotskyj@gmail.com>
2020-07-16 15:50:32 +0300

    fixed build

---
+++ model/amd.go
@@ -0,0 +1,13 @@
+package model
+
+type AmdParameters struct {
+	SilenceThreshold     *int `json:"silenceThreshold"`
+	MaximumWordLength    *int `json:"maximumWordLength"`
+	MaximumNumberOfWords *int `json:"maximumNumberOfWords"`
+	BetweenWordsSilence  *int `json:"betweenWordsSilence"`
+	MinWordLength        *int `json:"minWordLength"`
+	TotalAnalysisTime    *int `json:"totalAnalysisTime"`
+	AfterGreetingSilence *int `json:"afterGreetingSilence"`
+	Greeting             *int `json:"greeting"`
+	InitialSilence       *int `json:"initialSilence"`
+}
