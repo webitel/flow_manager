538a5adfcbea95054433032f386acf7762af1286
navrotskyj <navrotskyj@gmail.com>
2021-07-29 12:44:20 +0300

    WTEL-1984

--- store/pg_store/chat_store.go
+++ store/pg_store/chat_store.go
@@ -20,23 +20,23 @@ func (s SqlChatStore) RoutingFromProfile(domainId, profileId int64) (*model.Rout
 	var routing *model.Routing
 
 	err := s.GetReplica().SelectOne(&routing, `select
-		p.id as source_id,
-		p.name as source_name,
-		'' as source_data,
-		p.domain_id as domain_id,
-		d.name as domain_name,
-		coalesce(d.timezone_id, 287) timezone_id,
-		coalesce(ct.sys_name, 'UTC') as timezone_name,
-		p.schema_id as scheme_id,
-		ars.name as scheme_name,
-		ars.updated_at as schema_updated_at,
-		ars.debug,
-		null as variables
-	from chat.profile p
-		inner join flow.acr_routing_scheme ars on ars.id = p.schema_id
-		inner join directory.wbt_domain d on d.dc = p.domain_id
-		left join flow.calendar_timezones ct on d.timezone_id = ct.id
-	where p.id = :ProfileId and p.domain_id = :DomainId`, map[string]interface{}{
+			p.id as source_id,
+			p.name as source_name,
+			'' as source_data,
+			p.dc as domain_id,
+			d.name as domain_name,
+			coalesce(d.timezone_id, 287) timezone_id,
+			coalesce(ct.sys_name, 'UTC') as timezone_name,
+			p.flow_id as scheme_id,
+			ars.name as scheme_name,
+			ars.updated_at as schema_updated_at,
+			ars.debug,
+			null as variables
+		  from chat.bot p
+			inner join flow.acr_routing_scheme ars on ars.id = p.flow_id
+			inner join directory.wbt_domain d on d.dc = p.dc
+			left join flow.calendar_timezones ct on d.timezone_id = ct.id
+		  where p.id = :ProfileId and p.dc = :DomainId`, map[string]interface{}{
 		"ProfileId": profileId,
 		"DomainId":  domainId,
 	})
