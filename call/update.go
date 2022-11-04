7375767ac96801ae33f3273cd3f9d8c95465d330
Igor Navrotskyj <navrotskyj@gmail.com>
2022-11-04 16:28:22 +0200

    WTEL-3052

--- call/update.go
+++ call/update.go
@@ -2,33 +2,43 @@ package call
 
 import (
 	"context"
+
 	"github.com/webitel/flow_manager/flow"
 	"github.com/webitel/flow_manager/model"
 )
 
 type UpdateCid struct {
-	Name   *string `json:"name"`
-	Number *string `json:"number"`
+	Name   string `json:"name"`
+	Number string `json:"number"`
 }
 
 func (r *Router) UpdateCid(ctx context.Context, scope *flow.Flow, call model.Call, args interface{}) (res model.Response, err *model.AppError) {
 	var argv UpdateCid
+	var name, number *string
 
 	if err = r.Decode(scope, args, &argv); err != nil {
 		return nil, err
 	}
 
-	if argv.Name == nil && argv.Number == nil {
+	if argv.Name == "" && argv.Number == "" {
 		return nil, ErrorRequiredParameter("UpdateCid", "name and number")
 	}
 
-	if res, err = call.UpdateCid(ctx, argv.Name, argv.Number); err != nil {
+	if res, err = call.UpdateCid(ctx, name, number); err != nil {
 		return nil, err
 	}
 
-	if err = r.fm.UpdateCallFrom(call.Id(), argv.Name, argv.Number); err != nil {
+	if argv.Number != "" {
+		number = &argv.Number
+	}
+
+	if argv.Name != "" {
+		name = &argv.Name
+	}
+
+	if err = r.fm.UpdateCallFrom(call.Id(), name, number); err != nil {
 		return nil, err
 	}
 
-	return
+	return model.CallResponseOK, nil
 }
