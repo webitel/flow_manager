3a959d6594f2c249e5bb0d8d6eff9816949d61e9
Igor Navrotskyj <navrotskyj@gmail.com>
2023-06-21 12:55:24 +0300

    ENPF-10

--- store/pg_store/call_store.go
+++ store/pg_store/call_store.go
@@ -184,12 +184,8 @@ with del_calls as materialized (
     from call_center.cc_calls c
         where c.hangup_at < now() - '1 sec'::interval
             and c.direction notnull
-            and not (exists(select 1 from call_center.cc_member_attempt att where att.id = c.attempt_id)
-            or case
-                    when c.parent_id notnull then exists(select 1
-                                                             from call_center.cc_calls cp
-                                                             where (cp.id = c.parent_id)
-                                                               ) else false end)
+            and not exists(select 1 from call_center.cc_calls cc where case when c.parent_id notnull then cc.id = c.parent_id else cc.parent_id = c.id and cc.hangup_at isnull end )
+            and not exists(select 1 from call_center.cc_member_attempt att where att.id = c.attempt_id )
     order by c.hangup_at asc
     for update skip locked
     limit 1000
