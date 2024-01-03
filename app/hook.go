fbb8443646150ad5028586acef55982c9126f7b1
Igor Navrotskyj <navrotskyj@gmail.com>
2024-01-03 16:16:05 +0200

    WTEL-4017

--- app/hook.go
+++ app/hook.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/webitel/flow_manager/model"
 	"golang.org/x/sync/singleflight"
 )
@@ -8,5 +10,20 @@ import (
 var hookGroup singleflight.Group
 
 func (f *FlowManager) GetHookById(id string) (hook model.WebHook, err *model.AppError) {
-	return f.Store.WebHook().Get(id)
+
+	v, err2, _ := hookGroup.Do(id, func() (interface{}, error) {
+		return f.Store.WebHook().Get(id)
+	})
+
+	if err != nil {
+		switch err2.(type) {
+		case *model.AppError:
+			return hook, err2.(*model.AppError)
+		default:
+			return hook, model.NewAppError("Hook", "hook.settings.get", nil, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	return v.(model.WebHook), nil
+
 }
