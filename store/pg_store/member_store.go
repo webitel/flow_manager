364b3707424b4f73e6f4e0570aebc907a5ab1308
i.navrotskyj <i.navrotskyj@gmail.com>
2024-06-05 17:02:03 +0300

    GNG-28

--- store/pg_store/member_store.go
+++ store/pg_store/member_store.go
@@ -243,7 +243,7 @@ from m`, map[string]interface{}{
 
 func (s SqlMemberStore) CreateMember(domainId int64, queueId int, holdSec int, member *model.CallbackMember) *model.AppError {
 	_, err := s.GetMaster().Exec(`insert into call_center.cc_member(queue_id, communications, name, variables, 
-	ready_at, domain_id, timezone_id, priority, bucket_id, expire_at, agent_id)
+	ready_at, domain_id, timezone_id, priority, bucket_id, expire_at, agent_id, stop_cause)
 select q.id queue_id, 
 	   json_build_array(
               jsonb_build_object('destination', :Number::varchar)
@@ -260,7 +260,8 @@ select q.id queue_id,
 	   :Priority,
 	   :BucketId,
 	   case when :ExpireAt::int8 notnull and :ExpireAt::int8 > 0 then to_timestamp(:ExpireAt::int8/1000::double precision) at time zone tz.sys_name end,
-	   :AgentId
+	   :AgentId,
+	   :StopCause
 from call_center.cc_queue q
 	inner join flow.calendar c on c.id = q.calendar_id
     inner join flow.calendar_timezones tz on tz.id = c.timezone_id
@@ -280,6 +281,7 @@ where q.id = :QueueId::int4 and q.domain_id = :DomainId::int8`, map[string]inter
 		"ExpireAt":              member.ExpireAt,
 		"AgentId":               member.Agent.Id,
 		"CommunicationPriority": member.Communication.Priority,
+		"StopCause":             member.StopCause,
 	})
 
 	if err != nil {
