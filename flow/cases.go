59d3858ed8395ead907b007e2b5141097a2f7e2a
VolodiaHunkalo <graperz123@gmail.com>
2025-03-24 11:19:57 +0200

    [WTEL-6552]

--- flow/cases.go
+++ flow/cases.go
@@ -4,25 +4,26 @@ import (
 	"context"
 	"encoding/json"
 
-	cases_pb "buf.build/gen/go/webitel/cases/protocolbuffers/go"
+	pb "buf.build/gen/go/webitel/cases/protocolbuffers/go"
 	"github.com/webitel/flow_manager/model"
-	"github.com/webitel/wlog"
 )
 
 // ---------------------//
 // ** protobuf types ** //
 // ---------------------//
 type (
-	SearchCasesRequest       = cases_pb.SearchCasesRequest
-	LocateCaseRequest        = cases_pb.LocateCaseRequest
-	CreateCaseRequest        = cases_pb.CreateCaseRequest
-	UpdateCaseRequest        = cases_pb.UpdateCaseRequest
-	LinkCommunicationRequest = cases_pb.LinkCommunicationRequest
+	SearchCasesRequest       = pb.SearchCasesRequest
+	LocateCaseRequest        = pb.LocateCaseRequest
+	CreateCaseRequest        = pb.CreateCaseRequest
+	UpdateCaseRequest        = pb.UpdateCaseRequest
+	LinkCommunicationRequest = pb.LinkCommunicationRequest
+	GetServiceCatalogRequest = pb.ListCatalogRequest
 )
 
 // ----------------//
 // ** Arguments ** //
 // ----------------//
+
 type GetCasesArgs struct {
 	SearchCasesRequest
 	Token  string
@@ -53,15 +54,22 @@ type LinkCommunicationArgs struct {
 	SetVar string
 }
 
+type GetServiceCatalogsArgs struct {
+	GetServiceCatalogRequest
+	Token  string
+	SetVar string
+}
+
 // --------------------//
 // ** Function names **//
 // --------------------//
 const (
-	funcGetCases          = "getCases"
-	funcLocateCase        = "locateCase"
-	funcCreateCase        = "createCase"
-	funcUpdateCase        = "updateCase"
-	funcLinkCommunication = "linkCommunication"
+	funcGetCases           = "getCases"
+	funcLocateCase         = "locateCase"
+	funcCreateCase         = "createCase"
+	funcUpdateCase         = "updateCase"
+	funcLinkCommunication  = "linkCommunication"
+	funcGetServiceCatalogs = "getServiceCatalogs"
 )
 
 // ** Get Cases **
@@ -77,7 +85,6 @@ func (r *router) getCases(ctx context.Context, scope *Flow, conn model.Connectio
 
 	res, err := r.fm.SearchCases(ctx, &argv.SearchCasesRequest, argv.Token)
 	if err != nil {
-		logError(scope, conn, err)
 		return nil, model.NewAppError(funcGetCases, "get_cases_failed", nil, err.Error(), 500)
 	}
 
@@ -97,7 +104,6 @@ func (r *router) locateCase(ctx context.Context, scope *Flow, conn model.Connect
 
 	res, err := r.fm.LocateCase(ctx, &argv.LocateCaseRequest, argv.Token)
 	if err != nil {
-		logError(scope, conn, err)
 		return nil, model.NewAppError(funcLocateCase, "locate_case_failed", nil, err.Error(), 500)
 	}
 
@@ -117,7 +123,6 @@ func (r *router) createCase(ctx context.Context, scope *Flow, conn model.Connect
 
 	res, err := r.fm.CreateCase(ctx, &argv.CreateCaseRequest, argv.Token)
 	if err != nil {
-		logError(scope, conn, err)
 		return nil, model.NewAppError(funcCreateCase, "create_case_failed", nil, err.Error(), 500)
 	}
 
@@ -137,7 +142,6 @@ func (r *router) updateCase(ctx context.Context, scope *Flow, conn model.Connect
 
 	res, err := r.fm.UpdateCase(ctx, &argv.UpdateCaseRequest, argv.Token)
 	if err != nil {
-		logError(scope, conn, err)
 		return nil, model.NewAppError(funcUpdateCase, "update_case_failed", nil, err.Error(), 500)
 	}
 
@@ -150,18 +154,34 @@ func (r *router) linkCommunication(ctx context.Context, scope *Flow, conn model.
 	if err := scope.Decode(args, &argv); err != nil {
 		return nil, err
 	}
-
 	if err := checkRequiredFields(argv.Token, argv.SetVar, funcLinkCommunication); err != nil {
 		return nil, err
 	}
 
 	res, err := r.fm.LinkCommunication(ctx, &argv.LinkCommunicationRequest, argv.Token)
 	if err != nil {
-		logError(scope, conn, err)
 		return nil, model.NewAppError(funcLinkCommunication, "link_communication_failed", nil, err.Error(), 500)
 	}
 
 	return setResponse(ctx, conn, argv.SetVar, res)
+}
+
+func (r *router) getServiceCatalogs(ctx context.Context, scope *Flow, conn model.Connection, args any) (model.Response, *model.AppError) {
+	var argv GetServiceCatalogsArgs
+	if err := scope.Decode(args, &argv); err != nil {
+		return nil, err
+	}
+
+	if err := checkRequiredFields(argv.Token, argv.SetVar, funcGetServiceCatalogs); err != nil {
+		return nil, err
+	}
+
+	res, err := r.fm.GetServiceCatalogs(ctx, &argv.GetServiceCatalogRequest, argv.Token)
+	if err != nil {
+		return nil, model.NewAppError(funcGetServiceCatalogs, "get_service_catalogs_failed", nil, err.Error(), 500)
+	}
+	return setResponse(ctx, conn, argv.SetVar, res)
+
 }
 
 // -------------------//
@@ -171,11 +191,11 @@ func (r *router) linkCommunication(ctx context.Context, scope *Flow, conn model.
 // ** Helper function to check required fields and log errors **
 func checkRequiredFields(token, setVar, funcName string) *model.AppError {
 	if token == "" {
-		return model.NewAppError(funcName, "missing_token", nil, "Token is required", 400)
+		return model.ErrorRequiredParameter(funcName, "token")
 	}
 
 	if setVar == "" {
-		return model.NewAppError(funcName, "missing_set_var", nil, "SetVar is required", 400)
+		return model.ErrorRequiredParameter(funcName, "setVar")
 	}
 	return nil
 }
@@ -191,12 +211,4 @@ func setResponse(ctx context.Context, conn model.Connection, setVar string, res 
 	return conn.Set(ctx, model.Variables{
 		setVar: string(jsonData),
 	})
-}
-
-// ** Logging Helper Function **
-func logError(scope *Flow, conn model.Connection, err error) {
-	conn.Log().With(
-		wlog.Int("schema_id", scope.schemaId),
-		wlog.String("schema_name", scope.name),
-	).Error(err.Error())
 }
