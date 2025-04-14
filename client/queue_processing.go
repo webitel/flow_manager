8c6c337f56cf2dc86a85f529fc8acb9f81588217
i.navrotskyj <i.navrotskyj@gmail.com>
2025-04-14 13:08:23 +0300

    WTEL-6707; sync

--- client/queue_processing.go
+++ client/queue_processing.go
@@ -71,14 +71,14 @@ func (p *QueueProcessing) ActionForm(ctx context.Context, action string, vars ma
 	return p.form.Form, nil
 }
 
-func (p *QueueProcessing) ActionComponent(ctx context.Context, formId, componentId, action string, vars map[string]string) error {
+func (p *QueueProcessing) ActionComponent(ctx context.Context, formId, componentId, action string, vars map[string]string, sync bool) error {
 	_, err := p.cli.processing.ComponentAction(ctx, &workflow.ComponentActionRequest{
 		Id:          p.Id(),
 		FormId:      formId,
 		ComponentId: componentId,
 		Action:      action,
 		Variables:   vars,
-		Sync:        false,
+		Sync:        sync,
 	})
 
 	if err != nil {
