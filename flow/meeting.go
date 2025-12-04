2e9b5e84cb78ddc9d29516f8a013d9e886e2027a
i.navrotskyj <i.navrotskyj@gmail.com>
2025-12-04 09:57:45 +0200

    WTEL-8269

---
+++ flow/meeting.go
@@ -0,0 +1,37 @@
+package flow
+
+import (
+	"context"
+	"github.com/webitel/flow_manager/model"
+)
+
+type MeetingArgs struct {
+	SetVar    string            `json:"setVar"`
+	Title     string            `json:"title,omitempty"`
+	ExpireSec int64             `json:"expireSec,omitempty"`
+	BasePath  string            `json:"basePath,omitempty"`
+	Variables map[string]string `json:"variables,omitempty"`
+}
+
+func (r *router) createMeeting(ctx context.Context, scope *Flow, conn model.Connection, args any) (model.Response, *model.AppError) {
+
+	var (
+		argv MeetingArgs
+	)
+	if err := scope.Decode(args, &argv); err != nil {
+		return model.CallResponseError, err
+	}
+
+	if argv.SetVar == "" {
+		return model.CallResponseError, ErrorRequiredParameter("createMeeting", "setVar")
+	}
+
+	url, err := r.fm.Meeting().CreateMeeting(ctx, argv.Title, int(argv.ExpireSec), argv.BasePath, argv.Variables)
+	if err != nil {
+		return model.CallResponseError, err
+	}
+
+	return conn.Set(ctx, model.Variables{
+		argv.SetVar: url,
+	})
+}
