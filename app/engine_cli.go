c3eb389ffdb473878dae1723260236c6ecb2bec3
i.navrotskyj <i.navrotskyj@gmail.com>
2026-03-09 15:11:34 +0200

    WTEL-7049; fix build

--- app/engine_cli.go
+++ app/engine_cli.go
@@ -12,12 +12,11 @@ func (fm *FlowManager) MakeCall(ctx context.Context, req model.OutboundCallReque
 	if fm.engineCallCli == nil {
 		return "", model.NewAppError("App", "MakeCall", nil, "engine client not initialized to make a call", http.StatusInternalServerError)
 	}
-	
-	protoReq, err:= parseOutboundCallRequest(&req)
+
+	protoReq, err := parseOutboundCallRequest(&req)
 	if err != nil {
 		return "", model.NewAppError("App", "MakeCall", nil, err.Error(), http.StatusBadRequest)
 	}
-	
 
 	res, err := fm.engineCallCli.Api.CreateCallNA(ctx, protoReq)
 	if err != nil {
@@ -30,21 +29,21 @@ func (fm *FlowManager) MakeCall(ctx context.Context, req model.OutboundCallReque
 func parseOutboundCallRequest(req *model.OutboundCallRequest) (*engine.CreateCallRequest, error) {
 	protoReq := &engine.CreateCallRequest{
 		Destination: req.Destination,
-		DomainId: req.DomainID,
+		DomainId:    req.DomainID,
 	}
 	if req.From != nil {
 		protoReq.From = &engine.CreateCallRequest_EndpointRequest{
 			AppId:     req.From.AppId,
-			Type: req.From.Type,
+			Type:      req.From.Type,
 			Id:        req.From.Id,
 			Extension: req.From.Extension,
 		}
 	}
-	
-if req.To!= nil {
+
+	if req.To != nil {
 		protoReq.From = &engine.CreateCallRequest_EndpointRequest{
 			AppId:     req.To.AppId,
-			Type: req.To.Type,
+			Type:      req.To.Type,
 			Id:        req.To.Id,
 			Extension: req.To.Extension,
 		}
@@ -53,23 +52,19 @@ if req.To!= nil {
 	if req.Params != nil {
 		protoReq.Params = &engine.CreateCallRequest_CallSettings{
 			Timeout:           req.Params.Timeout,
-			Audio:req.Params.Audio,
-			Video:req.Params.Video,
-			Screen:req.Params.Screen,
-			Record:req.Params.Record,
-			Variables:req.Params.Variables,
+			Variables:         req.Params.Variables,
 			Display:           req.Params.Display,
-			DisableStun:req.Params.DisableStun,
-			CancelDistribute:req.Params.CancelDistribute,
-			IsOnline: req.Params.IsOnline,
-			DisableAutoAnswer:req.Params.DisableAutoAnswer,
-			HideNumber:req.Params.HideNumber,
+			DisableStun:       req.Params.DisableStun,
+			CancelDistribute:  req.Params.CancelDistribute,
+			IsOnline:          req.Params.IsOnline,
+			DisableAutoAnswer: req.Params.DisableAutoAnswer,
+			HideNumber:        req.Params.HideNumber,
 		}
 	}
 	return protoReq, nil
 }
 
-func (fm *FlowManager) GenerateFeedback(ctx context.Context, domainId int64, sourceId string, source string, payload map[string]string) (string, *model.AppError) {
+func (fm *FlowManager) GenerateFeedback(ctx context.Context, domainId int64, sourceId, source string, payload map[string]string) (string, *model.AppError) {
 	if fm.engineFeedbackCli == nil {
 		return "", model.NewAppError("App", "GenerateFeedback", nil, "engine client not initialized to generate feedback", http.StatusInternalServerError)
 	}
