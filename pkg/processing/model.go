5caaf2a6198718ce1e35e435fddc8f18ca6fdf77
i.navrotskyj <i.navrotskyj@gmail.com>
2026-04-15 09:22:13 +0300

    WTEL-8611

--- pkg/processing/model.go
+++ pkg/processing/model.go
@@ -7,35 +7,37 @@ import (
 
 type FormAction struct {
 	Name   string
-	Fields map[string]interface{}
+	Fields map[string]any
 }
 
-type JsonView map[string]interface{}
+type JsonView map[string]any
 
 type FormTableActionFn func(context.Context, bool, map[string]any) error
+
+type TableView struct {
+	Source           []any  `json:"source"`
+	IsSystemSource   bool   `json:"isSystemSource"`
+	DefaultCollapsed bool   `json:"defaultCollapsed"`
+	HeaderTitle      string `json:"headerTitle"`
+	SystemSource     struct {
+		Path string `json:"path"`
+		Name string `json:"name"`
+	} `json:"systemSource"`
+	DisplayColumns []struct {
+		Name  string `json:"name"`
+		Field string `json:"field"`
+		Type  string `json:"type"`
+		Width string `json:"width"`
+	} `json:"displayColumns"`
+}
 
 type FormTable struct {
 	Id        string                       `json:"id"`
 	OutputsFn map[string]FormTableActionFn `json:"-"`
 
 	View *struct {
-		Component string `json:"component"`
-		Table     struct {
-			Source           []any  `json:"source"`
-			IsSystemSource   bool   `json:"isSystemSource"`
-			DefaultCollapsed bool   `json:"defaultCollapsed"`
-			HeaderTitle      string `json:"headerTitle"`
-			SystemSource     struct {
-				Path string `json:"path"`
-				Name string `json:"name"`
-			} `json:"systemSource"`
-			DisplayColumns []struct {
-				Name  string `json:"name"`
-				Field string `json:"field"`
-				Type  string `json:"type"`
-				Width string `json:"width"`
-			} `json:"displayColumns"`
-		} `json:"table"`
+		Component string    `json:"component"`
+		Table     TableView `json:"table"`
 
 		Fields  []string `json:"fields"`
 		Filters []string `json:"filters"`
@@ -59,14 +61,14 @@ type FormView struct {
 	Options     []struct {
 		Name  string `json:"name"`
 		Value string `json:"value"`
-	} `json:"options,omitempty"` //wt-select
-	Multiple      bool   `json:"multiple,omitempty"`      //wt-select
-	Color         string `json:"color,omitempty"`         //form-text
-	Collapsible   bool   `json:"collapsible,omitempty"`   //form-text
-	EnableCopying bool   `json:"enableCopying,omitempty"` //form-text
+	} `json:"options,omitempty"` // wt-select
+	Multiple      bool   `json:"multiple,omitempty"`      // wt-select
+	Color         string `json:"color,omitempty"`         // form-text
+	Collapsible   bool   `json:"collapsible,omitempty"`   // form-text
+	EnableCopying bool   `json:"enableCopying,omitempty"` // form-text
 	Output        string `json:"output,omitempty"`        // "rich-text-editor"
-	Height        any    `json:"height,omitempty"`        //form-i-frame
-	Variable      string `json:"variable,omitempty"`      //form-select-from-object
+	Height        any    `json:"height,omitempty"`        // form-i-frame
+	Variable      string `json:"variable,omitempty"`      // form-select-from-object
 	Object        *struct {
 		Fields []string `json:"fields"`
 		Source struct {
@@ -75,13 +77,14 @@ type FormView struct {
 		} `json:"source"`
 		DisplayColumn string   `json:"displayColumn"`
 		Filters       []string `json:"filters"`
-	} `json:"object,omitempty"` //form-select-from-object
+	} `json:"object,omitempty"` // form-select-from-object
+	Table *TableView `json:"table"`
 }
 
 type FormComponent struct {
-	Id    string      `json:"id"`
-	View  *FormView   `json:"view"`
-	Value interface{} `json:"value"`
+	Id    string    `json:"id"`
+	View  *FormView `json:"view"`
+	Value any       `json:"value"`
 }
 
 type FormActionElem struct {
@@ -93,7 +96,7 @@ type FormElem struct {
 	Id      string            `json:"id"`
 	Title   string            `json:"title"`
 	Actions []*FormActionElem `json:"actions"`
-	Body    []interface{}     `json:"body"`
+	Body    []any             `json:"body"`
 }
 
 func (f *FormElem) ToJson() []byte {
