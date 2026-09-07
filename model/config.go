f35e359bb0e0bc205d9c65e61e0bcd0938cd31a2
vhunkaloo <v.hunkalo@webitel.com>
2026-09-07 10:26:42 +0300

    fix(im): restart owner bot on grant, never from inbound message

--- model/config.go
+++ model/config.go
@@ -9,7 +9,7 @@ type Config struct {
 	Id          string  `json:"id" flag:"id|1|Service id" env:"ID"`
 	ExternalSql bool    `json:"external_sql" flag:"external_sql|false|Enable external sql query" env:"EXTERNAL_SQL"`
 	AllowUseMQ  bool    `json:"allow_use_mq" flag:"allow_use_mq|false|Allow push message to MQ" env:"ALLOW_USE_MQ"`
-	Record      struct {
+	Record      struct{
 		Sample int `json:"sample" flag:"record_sample|0|Set the sample rate of the recording" env:"RECORD_SAMPLE"`
 	} `json:"record"`
 	DebugImap                    bool              `json:"debug_imap" flag:"debug_imap|false|Debug IMAP protocol" env:"DEBUG_IMAP"`
